128ca524525c155985597e8f0bb2fda7fd912c41
VividVice <vasiliy.novikov@epitech.eu>
2022-06-23 12:52:35 +0200

    ых

--- Bomb/Assets/Scripts/TimerCooldown.cs
+++ Bomb/Assets/Scripts/TimerCooldown.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TimerCooldown : MonoBehaviour
 {
-    public GameObject textDisplay;
+    public TextMeshProUGUI textDisplay;
     public int secondsleft = 30;
     public bool takingAway = false;
 
     void Start()
     {
-        textDisplay.GetComponent <TMPro.TextMeshProUGUI>().text = "00:" + secondsleft;
+        secondsleft = 30;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        textDisplay.text = "00:" + secondsleft.ToString();
     }
     // Start is called before the first frame update
     void Update()
     {
-        if(takingAway == false&& secondsleft > 0)
+        if(takingAway == false && secondsleft > 0)
         {
             StartCoroutine(TimerTake());
         }
@@ -26,7 +33,7 @@ public class TimerCooldown : MonoBehaviour
         takingAway = true;
         yield return new WaitForSeconds(secondsleft);
         secondsleft -= 1;
-        textDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "00:" + secondsleft;
+        SetCountText();
         takingAway = false;
     }
 }
